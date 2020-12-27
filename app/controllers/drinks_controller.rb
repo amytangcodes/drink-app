d60d3011b10a9858638dfa4a5f495f377c4152ff
Amy <amytang77@gmail.com>
2020-12-27 18:04:39 -0500

    added drinks and ingredients controller | show drink/:id

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,8 +1,18 @@
 class DrinksController < ApiController
 
+  # GET /drinks
   def index
-    @drinks = Drink.all
+    # @drinks = Drink.all  // this would select ALL information available to each drink
+    @drinks = Drink.select("id, title").all  # let's grab only the id and title for each drink
     render json: @drinks.to_json
+  end
+
+  # GET /drinks/:id
+  def show
+    # @drink = Drink.select("id, title", "description").find(params[:id]) # if you want to be selective on what to show
+
+    @drink = Drink.find(params[:id])
+    render json: @drink.to_json(:include => { :ingredients => { :only => [:id, :description] }})
   end
 
 end
