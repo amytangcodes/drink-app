0d324b947b012d5d0bf6ff6606c5a27ce5a1d019
Amy <amytang77@gmail.com>
2020-12-28 16:58:41 -0500

    adding react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
+import NotFound from "./components/NotFound";
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
 };
 export default App;
